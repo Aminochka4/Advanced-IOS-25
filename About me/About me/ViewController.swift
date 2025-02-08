de9fd6c97b53c3caffd99c068186bd0c44666f2a
Amina Amanzholova <eomino1607@gmail.com>
2025-02-08 21:24:27 +0500

    1

---
+++ About me/About me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About me
+//
+//  Created by Амина Аманжолова on 06.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
