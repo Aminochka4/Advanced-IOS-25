0007b313ce0f9c13a47978dc79e1109dabc90899
Amina Amanzholova <eomino1607@gmail.com>
2025-03-04 13:49:04 +0500

    finished lab3

---
+++ Hero/Hero/HeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroApp.swift
+//  Hero
+//
+//  Created by Амина Аманжолова on 02.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct HeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
